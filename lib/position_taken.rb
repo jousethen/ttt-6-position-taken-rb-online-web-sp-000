7f7a42ee263ab44c586ae3ce4120d045d2f1d54b
Jouse then <jthen93@gmail.com>
2021-02-06 20:11:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,6 @@
 def position_taken?(board, index)
   if board[index] == "" || board [index] == " " || board[index] == nil
     return FALSE
-  end
   elsif board[index] == "X" || board[index] == "O"
     return true
   end
